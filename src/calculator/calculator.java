ccad26a1d386ead961f8bd253d8b913631814f85
ClimbGoldy <44199745+ClimbGoldy@users.noreply.github.com>
2019-05-20 18:56:29 +0900

    what is this?

---
+++ src/calculator/calculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class calculator {
+    public int sum(int num1, int num2){
+        return num1 + num2;
+    }
+    public int sub(int num1, int num2){
+        return num1 - num2;
+    }
+    public int mul(int num1, int num2){
+        return num1 * num2;
+    }
+    public int div(int num1, int num2){
+        return num1 / num2;
+    }
+}
