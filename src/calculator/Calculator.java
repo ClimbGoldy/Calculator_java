2835578ddc169a1503a628740a2108e6d9d11c74
ClimbGoldy <44199745+ClimbGoldy@users.noreply.github.com>
2019-05-20 21:07:47 +0900

    Update Calculator.java

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,16 +1,24 @@
 package calculator;
 
 public class Calculator {
-    public int sum(int num1, int num2){
-        return num1 + num2;
-    }
-    public int sub(int num1, int num2){
-        return num1 - num2;
-    }
-    public int mul(int num1, int num2){
-        return num1 * num2;
-    }
-    public int div(int num1, int num2){
-        return num1 / num2;
-    }
+	public int sum(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public int sub(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	public int mul(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	public int div(int num1, int num2) {
+		try {
+			return num1 / num2;
+		}catch(Exception e){
+			//divide By 0 exception
+		}
+		return 0;
+	}
 }
